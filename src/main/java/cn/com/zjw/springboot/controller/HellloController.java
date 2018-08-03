0fbb8382dc3d15a3de3e9d45b23e7b4165a4268c
zhoujiawei <949690115@qq.com>
2018-08-03 16:37:17 +0800

    springboot框架基础修改

--- src/main/java/cn/com/zjw/springboot/controller/HellloController.java
+++ src/main/java/cn/com/zjw/springboot/controller/HellloController.java
@@ -2,14 +2,13 @@ package cn.com.zjw.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HellloController {
 
     @RequestMapping("/hello")
-    @ResponseBody
+//    @ResponseBody
     public String sayHello() {
-        return "Hello World";
+        return "test";
     }
 }

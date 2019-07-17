08b44b4ceabb4e0f705cc3ada4b9105398a6785b
zhoujiawei <949690115@qq.com>
2019-07-17 10:32:50 +0800

    登陆

--- src/main/java/cn/com/zjw/springboot/controller/LoginController.java
+++ src/main/java/cn/com/zjw/springboot/controller/LoginController.java
@@ -1,5 +1,6 @@
 package cn.com.zjw.springboot.controller;
 
+import cn.com.zjw.springboot.dto.MenuDto;
 import cn.com.zjw.springboot.entity.Menu;
 import cn.com.zjw.springboot.entity.User;
 import cn.com.zjw.springboot.service.MenuService;
@@ -8,13 +9,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 登陆
@@ -63,14 +62,63 @@ public class LoginController extends BaseController {
                 return fail("用户名或密码错误");
             }
 
-            // 返回用户菜单
+            // 返回用户的菜单
             List<Menu> menuList = menuService.getUserMenu(user.getId());
-            return success(menuList);
+            // 复制返回用户的菜单
+            List<Menu> coypMenus = new ArrayList<>();
+            coypMenus.addAll(menuList);
+            // 所有一级菜单
+            List<MenuDto> addList = new ArrayList<>();
+            for (Menu menu : menuList) {
+                if (StringUtils.isBlank(menu.getPid())) {
+                    MenuDto menuDto = new MenuDto();
+                    menuDto.setId(menu.getId());
+                    menuDto.setName(menu.getMenuName());
+                    addList.add(menuDto);
+
+                    coypMenus.remove(menu);
+
+                    recursionMenu(coypMenus, addList, menuDto, menu.getId());
+                }
+            }
+
+            return success(addList);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             return fail(e.getMessage());
         }
-
-
     }
+
+    /**
+     * 递归生成正确的菜单树
+     * @author zhoujiawei
+     * @param list 未设置的菜单集合
+     * @param addList 设置层的菜单集合
+     * @param menuDto 设置层的菜单对象
+     * @param menuId 设置层的菜单id
+     * @return
+     */
+    private List<MenuDto> recursionMenu(List<Menu> list, List<MenuDto> addList, MenuDto menuDto, String menuId) {
+        List<Menu> copyMenus = new ArrayList<>();
+        copyMenus.addAll(list);
+        for (Menu menu : list) {
+            if (menuId.equals(menu.getPid())) {
+                List<MenuDto> subMenus = menuDto.getSubMenus();
+                MenuDto subMenuDto = new MenuDto();
+                subMenuDto.setId(menu.getId());
+                subMenuDto.setName(menu.getMenuName());
+                subMenuDto.setUrl(menu.getMenuRoutePath());
+                subMenus.add(subMenuDto);
+                menuDto.setSubMenus(subMenus);
+
+                copyMenus.remove(menu);
+
+                recursionMenu(copyMenus, subMenus, subMenuDto, menu.getId());
+
+            }
+        }
+        return addList;
+    }
+
+
 }
---
+++ src/main/java/cn/com/zjw/springboot/dto/MenuDto.java
@@ -0,0 +1,49 @@
+package cn.com.zjw.springboot.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuDto implements Serializable {
+
+
+    private String id;
+
+    private String name;
+
+    private String url;
+
+    private List<MenuDto> subMenus = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<MenuDto> getSubMenus() {
+        return subMenus;
+    }
+
+    public void setSubMenus(List<MenuDto> subMenus) {
+        this.subMenus = subMenus;
+    }
+}
